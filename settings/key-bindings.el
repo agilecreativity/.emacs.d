51599c7d70f31f68c7f63ee4748a493b61c32d28
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-25 12:14:16 +1000

    Adjust/remove some key bindings

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -24,23 +24,26 @@
 
 ;; Experimental multiple-cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
+(global-set-key (kbd "C-S-c C-e")   'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-S-c C-a")   'mc/edit-beginnings-of-lines)
 
 ;; Mark additional regions matching current region
-(global-set-key (kbd "M-æ") 'mc/mark-all-dwim)
-(global-set-key (kbd "C-å") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-æ") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-Æ") 'mc/mark-more-like-this-extended)
-(global-set-key (kbd "M-å") 'mc/mark-all-in-region)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 
-;; Symbol and word specific mark-more
-(global-set-key (kbd "s-æ") 'mc/mark-next-word-like-this)
-(global-set-key (kbd "s-å") 'mc/mark-previous-word-like-this)
-(global-set-key (kbd "M-s-æ") 'mc/mark-all-words-like-this)
-(global-set-key (kbd "s-Æ") 'mc/mark-next-symbol-like-this)
-(global-set-key (kbd "s-Å") 'mc/mark-previous-symbol-like-this)
-(global-set-key (kbd "M-s-Æ") 'mc/mark-all-symbols-like-this)
+; (global-set-key (kbd "M-æ") 'mc/mark-all-dwim)
+; (global-set-key (kbd "C-Æ") 'mc/mark-more-like-this-extended)
+; (global-set-key (kbd "M-å") 'mc/mark-all-in-region)
+
+;; Word specific
+; (global-set-key (kbd "s-æ")   'mc/mark-next-word-like-this)
+; (global-set-key (kbd "s-å")   'mc/mark-previous-word-like-this)
+; (global-set-key (kbd "M-s-æ") 'mc/mark-all-words-like-this)
+
+;; Symbol specific
+; (global-set-key (kbd "s-Æ")   'mc/mark-next-symbol-like-this)
+; (global-set-key (kbd "s-Å")   'mc/mark-previous-symbol-like-this)
+; (global-set-key (kbd "M-s-Æ") 'mc/mark-all-symbols-like-this)
 
 ;; Extra multiple cursors stuff
 (global-set-key (kbd "C-~") 'mc/reverse-regions)
@@ -55,8 +58,8 @@
 ;; Replace rectangle-text with inline-string-rectangle
 (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
-;; Quickly jump in document with ace-jump-mode
-(define-key global-map (kbd "C-ø") 'ace-jump-mode)
+;; Quickly jump in document with ace-jump-mode)
+;(define-key global-map (kbd "C-ø") 'ace-jump-mode)
 
 ;; Perform general cleanup.
 (global-set-key (kbd "C-c n") 'cleanup-buffer)
